d8a514c66a71692444ab7d5009af9f595a73f13d
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-06 23:55:17 +0300

    Update img-wanted.js

--- plugins/img-wanted.js
+++ plugins/img-wanted.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `> *Powered by David X*`
+      caption: `> *Powered by HACKLINK TECH.INC*`
     });
 
   } catch (error) {
