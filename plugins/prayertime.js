012d78c8885b15b3f5d5a7b5ebc44ee4086154e3
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-07 01:27:17 +0300

    Update prayertime.js

--- plugins/prayertime.js
+++ plugins/prayertime.js
@@ -60,8 +60,8 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363400583993139@newsletter',
+                        newsletterName: 'HACKLINK',
                         serverMessageId: 143
                     }
                 }
