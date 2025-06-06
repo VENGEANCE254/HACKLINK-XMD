5abf039d058b0d9ca10e7be93ef96abafce10bef
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-06 23:08:54 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "JAWAD-MD",
+            author: "HACKLINK TECH.INC",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
