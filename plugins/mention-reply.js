a85d8d4dcac49f5d8a62460da3ff59d28646d7b7
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-07 00:57:08 +0300

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -26,7 +26,7 @@ cmd({
     const botNumber = conn.user.id.split(":")[0] + '@s.whatsapp.net';
 
     if (m.mentionedJid.includes(botNumber)) {
-      const thumbnailRes = await axios.get(config.MENU_IMAGE_URL || "https://files.catbox.moe/c836ws.png", {
+      const thumbnailRes = await axios.get(config.MENU_IMAGE_URL || "https://files.catbox.moe/91amrd.jpg", {
         responseType: 'arraybuffer'
       });
       const thumbnailBuffer = Buffer.from(thumbnailRes.data, 'binary');
@@ -40,13 +40,13 @@ cmd({
           forwardingScore: 999,
           isForwarded: true,
           externalAdReply: {
-            title: config.BOT_NAME || "KHAN-MD 🥀",
-            body: config.DESCRIPTION || "POWERED BY JAWAD TECHX 🤌💗",
+            title: config.BOT_NAME || "HACKLINK-XMD",
+            body: config.DESCRIPTION || "POWERED BY HACKLINK TECH.INC*",
             mediaType: 1,
             renderLargerThumbnail: true,
             thumbnail: thumbnailBuffer,
-            mediaUrl: "https://files.catbox.moe/l2t3e0.jpg", // Static image URL
-            sourceUrl: "https://wa.me/message/INB2QVGXHQREO1",
+            mediaUrl: "https://files.catbox.moe/91amrd.jpg", // Static image URL
+            sourceUrl: "https://wa.me/message/254769677305",
             showAdAttribution: true
           }
         }
