0bbd05307702ed4cc66203d5f3d0a8720b47d5bb
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-07 01:21:05 +0300

    Update npm-search.js

--- plugins/npm-search.js
+++ plugins/npm-search.js
@@ -33,7 +33,7 @@ cmd({
 
     // Create the response message
     const message = `
-*KHAN-MD NPM SEARCH*
+*HACKLINK TECH.INC NPM SEARCH*
 
 *🔰 NPM PACKAGE:* ${packageName}
 *📄 DESCRIPTION:* ${description}
