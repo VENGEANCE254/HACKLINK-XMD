2c193c33dea8f32e14d9fb46baf2b9e75ff0f5fe
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-07 01:17:46 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -54,7 +54,7 @@ async (conn, mek, m, { from, reply, sender, args }) => {
             from,
             {
                 image: { 
-                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster : 'https://files.catbox.moe/7zfdcq.jpg'
+                    url: movie.poster && movie.poster !== 'N/A' ? movie.poster : 'https://files.catbox.moe/elhfta.jpg'
                 },
                 caption: dec,
                 contextInfo: {
@@ -62,8 +62,8 @@ async (conn, mek, m, { from, reply, sender, args }) => {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363400583993139@newsletter',
+                        newsletterName: 'HACKLINK',
                         serverMessageId: 143
                     }
                 }
