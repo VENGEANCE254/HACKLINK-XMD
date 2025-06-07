9b0ad7d19b6f864fc99337e139e670925a8a4402
VENGEANCE254 <hacklinka7@gmail.com>
2025-06-07 10:53:00 +0300

    Update tool-vfc.js

--- plugins/tool-vfc.js
+++ plugins/tool-vfc.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
-const config = require('../config')
-const { cmd, commands } = require('../command')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('../lib/functions');
 
-
-
-//vcf//
-
+// vcf //
 cmd({
     pattern: 'savecontact',
-    alias: ["vcf","scontact","savecontacts"],
+    alias: ["vcf", "scontact", "savecontacts"],
     desc: 'gc vcard',
     category: 'tools',
     filename: __filename
@@ -18,14 +15,14 @@ cmd({
         if (!isGroup) return reply("This command is for groups only.");
         if (!isOwner) return reply("*_This command is for the owner only_*");
 
-        let card = quoted || m; // Handle if quoted message exists
+        let card = quoted || m;
         let cmiggc = groupMetadata;
         const { participants } = groupMetadata;
-        
+
         let orgiggc = participants.map(a => a.id);
         let vcard = '';
         let noPort = 0;
-        
+
         for (let a of cmiggc.participants) {
             vcard += `BEGIN:VCARD\nVERSION:3.0\nFN:[${noPort++}] +${a.id.split("@")[0]}\nTEL;type=CELL;type=VOICE;waid=${a.id.split("@")[0]}:+${a.id.split("@")[0]}\nEND:VCARD\n`;
         }
@@ -37,15 +34,14 @@ cmd({
         await sleep(2000);
 
         await conn.sendMessage(from, {
-            document: fs.readFileSync(nmfilect), 
-            mimetype: 'text/vcard', 
-            fileName: 'hacklink.vcf', 
-            caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ HACKLINK TECH.INC*'}, { quoted: mek });
+            document: fs.readFileSync(nmfilect),
+            mimetype: 'text/vcard',
+            fileName: 'hacklink.vcf',
+            caption: `\nDone saving.\nGroup Name: *${cmiggc.subject}*\nContacts: *${cmiggc.participants.length}*\n> ᴘᴏᴡᴇʀᴇᴅ ʙʏ HACKLINK TECH.INC*`
+        }, { quoted: mek });
 
         fs.unlinkSync(nmfilect); // Cleanup the file after sending
     } catch (err) {
         reply(err.toString());
     }
 });
-
-
